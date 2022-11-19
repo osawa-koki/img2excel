d43cc66129d3f62f5db49f13a27ac126b74147c9
osawa-koki <github@osawa.dev>
2022-11-20 01:56:12 +0900

    MainStreamまで。

---
+++ img2excel/MainStream.cs
@@ -0,0 +1,10 @@
+
+internal static partial class Program
+{
+  static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+  internal static void MainStream()
+  {
+      
+  }
+}
--- img2excel/Program.cs
+++ img2excel/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string mutexName = "img2excel";
+
+Mutex mutex = new(false, mutexName);
+bool hasHandle = false;
+try
+{
+  try
+  {
+    // ミューテックスの所有権を要求する
+    hasHandle = mutex.WaitOne(0, false);
+  }
+
+  catch (AbandonedMutexException)
+  {
+    // 別のアプリケーションがミューテックスを解放しないで終了した時
+    hasHandle = true;
+  }
+  // ミューテックスを得られたか調べる
+  if (hasHandle == false)
+  {
+    // 得られなかった場合は、すでに起動していると判断して終了
+    logger.Warn("既に別のインスタンスが実行されています。");
+    return 1;
+  }
+
+  logger.Info("実行開始");
+  MainStream();
+  logger.Info("実行完了");
+  return 0;
+}
+catch (Exception ex)
+{
+  logger.Error(ex);
+  return 1;
+}
+finally
+{
+  if (hasHandle)
+  {
+    // ミューテックスを解放する
+    mutex.ReleaseMutex();
+  }
+  mutex.Close();
+}
